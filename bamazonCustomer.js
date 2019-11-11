62f3f848f8b5456b27a4741df57999b4272a175e
admin <admin@admins-MacBook-Pro.local>
2019-11-11 11:34:03 -0800

    added my commit to the each line

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
 console.log("this is working");
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+// creating connection to the mysql and store in the variable "connection".//
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -15,7 +15,7 @@ var connection = mysql.createConnection({
   password: "yourRootPassword",
   database: "bamazon_db"
 });
-
+// function that establishing connection to database if there are throw error otherwise print information.//
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
@@ -23,7 +23,7 @@ connection.connect(function (err) {
   start();
 
 });
-
+//this function select the database table call "products" and print the table informations.//
 function afterConnection() {
   connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
@@ -31,6 +31,7 @@ function afterConnection() {
     connection.end();
   });
 }
+//this is start function that will prompt user for two messages 
 function start() {
   inquirer
     .prompt({
@@ -39,6 +40,7 @@ function start() {
       message: "what is product [item_id] your like to buy today? and how manny [quantity] you like to buy?",
       choices: ["item_id", "stock_quantity", "cancell"]
     })
+    //whenever user select different answer that will trigger differnt function base on thier choices and will end the connection with database.
      .then(function(answer) {
       if (answer.BuyOrNotbuy === "item_id") {
         itemId();
@@ -53,6 +55,7 @@ function start() {
     })
 
 }
-// function itemId(){
 
-// }
+function itemId(){
+
+}
