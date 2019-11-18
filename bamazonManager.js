4cdc474af7799cd9aa89a59f88c7143b4c40d942
admin <admin@admins-MacBook-Pro.local>
2019-11-18 12:52:30 -0800

    done working function for view products, view low inventory and add inventory

--- bamazonManager.js
+++ bamazonManager.js
@@ -37,17 +37,17 @@ function welcome() {
 
 
             }
-        ]).then(function(answer) {
-            if (answer.menuOption === " Product for sale"){
+        ]).then(function (answer) {
+            if (answer.menuOption === " Product for sale") {
                 viewProduct();
             }
-            if (answer.menuOption === "Low Inventory"){
+            if (answer.menuOption === "Low Inventory") {
                 viewInventory();
             }
-            if (answer.menuOption === "Add to Inventory"){
+            if (answer.menuOption === "Add to Inventory") {
                 addInventory();
             }
-            if (answer.menuOption ===  "Add new product"){
+            if (answer.menuOption === "Add new product") {
                 addNewProduct();
             }
 
@@ -71,16 +71,97 @@ function welcome() {
             // }
         })
 }
-function viewProduct(answer){
-    console.log("this is view product")
+function viewProduct(answer) {
+    var query = "SELECT * FROM products"
+    connection.query(query, function (err, results) {
+        if (err) throw err;
+        console.table(results);
+        welcome()
+
+    })
 }
 
-function viewInventory(answer){
-    console.log("this is view inventory")
+function viewInventory(answer) {
+    var query = "SELECT * FROM products  WHERE stock_quantity<10";
+    // run query to database //
+    connection.query(query, function (err, results) {
+        if (err) throw err;
+        console.log("---------Low Inventory level---------");
+        console.table(results);
+        welcome()
+
+    })
 }
-function addInventory(answer){
+function addInventory(answer) {
     console.log("adding to the inventory")
+    //query the database to show all the products in the inventtory so they can add more inventory with id number//
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        console.table(results)
+    })
+    // asking manager which id number product you like to add to the inventory//
+    console.log("\n=======================================================================================\n")
+    inquirer
+        .prompt([
+            {
+                name: "id",
+                type: "input",
+                message: "input item id to add your inventory",
+                // checking the item id is number greater than zero and contained in the DB//
+                validate: function (value) {
+                    if (isNaN(value) == false) {
+                        return true;
+                    }
+                    return false;
+                }
+
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: " what is the amount of quantity adding to the inventory?",
+                validate: function (value) {
+                    if (isNaN(value) == false) {
+                        return true;
+                    }
+                    return false;
+                }
+
+            }
+        ]).then(function (answer) {
+            var itemQuantity = 0;
+            for (var i = 0; i < results.length; i++) {
+                if (answer.id === results[i].item_id) {
+                    stock_quantity = results[i].stock_quantity + answer.quantity
+                }
+            }
+        })
+
+        // increseQty()
+        
+
 }
-function addNewProduct(answer){
+
+function increseQty(){
+    connection.query(
+		"UPDATE products SET ? WHERE ?", 
+		[
+			{
+				stock_quantity: stockQty + parseInt(addQty)
+			}, 
+			{
+				item_id: parseInt(item)
+			}
+		], 
+		function(error, results) {
+			// throw error, else log inventory updated and return to welcome screen
+			if (error) throw error;
+			console.log("\nInventory successfully increased.\n");
+			welcome();
+	});
+}
+
+}
+function addNewProduct(answer) {
     console.log("adding new product to the database");
 }
